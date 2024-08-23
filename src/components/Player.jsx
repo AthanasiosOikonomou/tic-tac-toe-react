61bb98aca2e4603a4bfd163d1bf077ed8ce3d9e1
AthanasiosOikonomou <ath.oikonomou@hotmail.com>
2024-08-23 19:23:35 +0300

    added gameover, log and winning-combination files. A lot of logic with the states changed too.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing((editing) => !isEditing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChange(event) {
@@ -23,7 +31,7 @@ export default function Player({ initialName, symbol }) {
   }
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {editablePLayerName}
         <span className="player-symbol">{symbol}</span>
